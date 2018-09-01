d14e01951e7449eee4bcc327b8d26861d32a0749
hesam <hesamr76@gmail.com>
2018-09-02 01:37:12 +0430

    make project on 8/2/2018

---
+++ assets/js/main.js
@@ -0,0 +1,83 @@
+let numberOfSites =  0 , current = 1 , center = 1;
+var sites = {"1":1,"2":2,"3":3}
+
+function setCenter(sites){
+  if (sites  % 2 == 0) {
+    return  sites / 2;
+  }else{
+    return (sites+1) / 2;
+  }
+}
+
+function define(){
+
+  $(".card").each(function(){
+    numberOfSites ++;
+  })
+
+  current = setCenter(numberOfSites);
+  center = current;
+
+  setCards()
+}
+
+function setCards() {
+  for (let i=1 ; i <= numberOfSites ; i++) {
+    locate(i,center);
+  }
+}
+
+function locate(index,center){
+  if(sites[index] == center){
+    active(index)
+  }else if (sites[index] < center) {
+    goLeft(index)
+  }else{
+    goRight(index)
+  }
+}
+
+function active(item){
+  $("#site_" + item).css({"width":"40%","left":"30%","margin-top":"2.5rem","z-index":"90","opacity":"1.0"})
+}
+
+function goRight(item) {
+  $("#site_" + item).css({"width":"30%","left":"60%","margin-top":"4.5rem","z-index":"80","opacity":"0.8"})
+}
+
+function goLeft(item){
+  $("#site_" + item).css({"width":"30%","left":"10%","margin-top":"4.5rem","z-index":"80","opacity":"0.8"})
+}
+
+function rightReorder() {
+  for (let i=1 ; i <= numberOfSites ; i++) {
+    if (sites[i] == 1) {
+      sites[i] = 2
+    }else if (sites[i] == 2) {
+      sites[i] = 3
+    }else {
+      sites[i] = 1
+    }
+  }
+  setCards()
+}
+
+function leftReorder() {
+  for (let i=1 ; i <= numberOfSites ; i++) {
+    if (sites[i] == 1) {
+      sites[i] = 3
+    }else if (sites[i] == 2) {
+      sites[i] = 1
+    }else {
+      sites[i] = 2
+    }
+  }
+  setCards()
+}
+
+function next(){current--;rightReorder()}
+
+function prev(){current++;leftReorder()}
+
+
+ define()
